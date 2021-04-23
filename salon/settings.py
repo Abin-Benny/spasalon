834d5e49c8b77cdac917ff7b201fdc08ffb1cefe
Abin Benny <abin95benny@gmail.com>
2021-04-23 16:42:14 +0530

    Salon and User login and registration tables created in models.py and migrated it

--- salon/settings.py
+++ salon/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'salon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'spasalon',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': ''
     }
 }
 
--- user/models.py
+++ user/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class salonlogin(models.Model):
+    Username=models.EmailField(max_length=50)
+    Password=models.CharField(max_length=20)
+   # Role=models.IntegerField(default=1)# 0=admin,1=user
+
+class salonreg(models.Model):
+    Login_id=models.ForeignKey(salonlogin,on_delete=models.CASCADE)
+    First_name=models.CharField(max_length=50)
+    Last_name=models.CharField(max_length=50)
+    Email=models.EmailField(max_length=50)
+    Password=models.CharField(max_length=20)
+    Mobile= models.CharField(max_length=20)
+    Address=models.CharField(max_length=250)
+
+class clientlogin(models.Model):
+    Username=models.EmailField(max_length=50)
+    Password=models.CharField(max_length=20)
+   # Role=models.IntegerField(default=1)# 0=admin,1=user
+
+class clientreg(models.Model):
+    Login_id=models.ForeignKey(clientlogin,on_delete=models.CASCADE)
+    First_name=models.CharField(max_length=50)
+    Last_name=models.CharField(max_length=50)
+    Email=models.EmailField(max_length=50)
+    Password=models.CharField(max_length=20)
+    Mobile= models.CharField(max_length=20)
+    Address=models.CharField(max_length=250)
