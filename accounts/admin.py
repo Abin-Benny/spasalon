c6739040a1ae6f216931bc1b2e9e7c1a52178bbe
Abin Benny <abin95benny@gmail.com>
2021-05-26 09:52:52 +0530

    category app is created and category models is created in models.py file of category app and category model is registered in admin.py file of category app.After migrations, category tab is created in the admin module.Under category tab,categorys(plural name) folder is created.so, we corrected the plural name from categorys to categories.For that class Meta is created inside category model of category app

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,11 +4,11 @@ from .models import Account
 
 # Register your models here.
 class AccountAdmin(UserAdmin):
-    list_display = ('email','first_name','last_name','username','last_login','date_joined','is_active') # Display all the items given in this line in a single line
+    list_display = ('email','first_name','last_name','username','last_login','date_joined','is_active') # Display all the items given in this line in a single line and make password field read only
     list_display_links = ('email','first_name','last_name') # It provide links to all items given in this line to inner page
     readonly_fields = ('last_login','date_joined') # user only can read not edit or write the items given in this fields
     ordering = ('-date_joined',) # display items in descending order of date joined.
-    filter_horizontal = () #this line and below two lines are used to display items provided in list_display in a single line
+    filter_horizontal = () #this line and below two lines are used to display items provided in list_display in a single line and make password field read only
     list_filter = ()
     fieldsets=()
 admin.site.register(Account,AccountAdmin)
