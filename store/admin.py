299a4a1c87ae4bfa2133fc8a12369bcb0b799e3b
Abin Benny <abin95benny@gmail.com>
2021-06-04 19:01:38 +0530

    Deleted category and store app

--- store/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import product
-
-# Register your models here.
-class productAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('product_name',)}
-    list_display =  ('product_name','price','stock','created_date','modified_date','is_available')
-
-admin.site.register(product, productAdmin)
--- store/storeurls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path('Products/',views.products,name="shop_home"),
-]
